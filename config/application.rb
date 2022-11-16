7c05d9873a16f7562f47eea48021694427c3524d
Stephen <stephen.charette@outlook.com>
2022-11-16 17:26:42 -0500

    init commit

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsTemplate # CHANGE_ME
+module StarPoints # CHANGE_ME
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
