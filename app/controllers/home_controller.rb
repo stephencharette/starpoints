c2ada719c496b31fd3cf140151bcccae1b8dbff9
Stephen <stephen.charette@outlook.com>
2022-11-22 19:39:09 -0500

    add tabs on the home page and fix layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @credit_cards = current_user.credit_cards.order(:created_at)
     @metrics = [['1-6', 1], ['7-13', 5], ['14-20', 6], ['21-27', 10], ['28-30', 12]]
   end
 end
